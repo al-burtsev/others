27d86d7eeb9b7f9302a00c46fe548bf08deb2a4e
@burtsev_aleksandr <burtsev.aleksandr9@gmail.com>
2023-04-22 17:37:04 +0300

    some practice

---
+++ gulpTraining/test/gulpfile.js
@@ -0,0 +1,71 @@
+
+const { src, dest, series, watch } = require('gulp');
+const concat = require('gulp-concat');
+const htmlMin = require('gulp-htmlmin');
+const autoPrefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const svgSprite = require('gulp-svg-sprite');
+const image = require('gulp-image');
+const browserSync = require('browser-sync').create()
+
+const styles = () => {
+    return src('src/styles/**/*.css')
+        .pipe(concat('main.css'))
+        .pipe(autoPrefixer({
+            cascade: false,
+        }))
+        .pipe(cleanCSS({
+            level: 2,
+        }))
+        .pipe(dest('dist'))
+        .pipe(browserSync.stream())
+}
+
+const htmlMinify = () => {
+
+    return src('src/**/*.html')
+        .pipe(htmlMin({
+            collapseWhitespace: true,
+        }))
+        .pipe(dest('dist'))
+        .pipe(browserSync.stream())
+}
+
+const svgSprites = () => {
+    return src('src/images/svg/**/*.svg')
+    .pipe(svgSprite({
+        mode: {
+            stack: {
+                sprite: '../sprite.svg'
+            }
+        }
+    }))
+    .pipe(dest('dist/images'))
+}
+
+const watchFiles = () => {
+    browserSync.init({
+        server: {
+            baseDir: 'dist'
+        }
+    })
+}
+
+const images = () => {
+    return src([
+        'src/images/**/*.jpg',
+        'src/images/**/*.jpeg',
+        'src/images/**/*.png',
+        'src/images/*.svg',
+    ])
+    .pipe(image())
+    .pipe(dest('dist/images'))
+    }
+
+watch('src/**/*.html', htmlMinify)
+watch('src/styles/**/*.css', styles)
+watch('src/images/**/*.svg', svgSprites)
+
+exports.styles = styles
+exports.htmlMinify = htmlMinify
+exports.default = series(htmlMinify, styles, images, svgSprites, watchFiles)
